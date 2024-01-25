7e467f475ac28a6442c4cdf7494f57a54f165d69
EfterFace <erykwykyk@gmail.com>
2024-01-25 00:56:58 +0000

    Init
    Init

---
+++ Source/TestingShooting/Public/NoneClassTest.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class TESTINGSHOOTING_API NoneClassTest
+{
+public:
+	NoneClassTest();
+	~NoneClassTest();
+};
